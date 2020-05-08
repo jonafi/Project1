0f2b0d521cb0e63c1af593cd87c258e496dc3f99
Jsquad <lakis006@umn.edu>
2020-05-07 21:17:40 -0500

    started building the ajax function

--- assets/food.js
+++ assets/food.js
@@ -1,19 +1,16 @@
 
 
 function getRecipe(userInput) {
-    
-    var apiKey = "";
-    var queryURL = "" + apiKey + x  + y;
+
+    var apiKey = "a7069c43e13c417486872a222fdec9b1";
+    var queryURL = "https://api.spoonacular.com/recipes/search?query=cheese&number=5&apiKey=" + apiKey;
       $.ajax({
   
           url: queryURL,
           method: "GET"
       }).then(function (response) {
-          var uvStuff = response.value;
           console.log("it works");
           console.log(response);
-          
-         
   
       })
     
