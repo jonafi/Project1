aa82732d59d6272198f88822f33896615013100c
Jsquad <lakis006@umn.edu>
2020-05-12 21:11:43 -0500

    commented out the unnecessary function

--- assets/food.js
+++ assets/food.js
@@ -1,75 +1,75 @@
-function getRecipe() {
+// function getRecipe() {
 
 
-    var queryURL = "https://www.themealdb.com/api/json/v1/1/random.php";
-    $.ajax({
+//     var queryURL = "https://www.themealdb.com/api/json/v1/1/random.php";
+//     $.ajax({
 
-        url: queryURL,
-        method: "GET"
-    }).then(function (response) {
+//         url: queryURL,
+//         method: "GET"
+//     }).then(function (response) {
         
-        var foodIngredients = [];
-        var foodMeasures = [];
-        var mealName = response.meals[0].strMeal;
-        var foodPicURL = response.meals[0].strMealThumb;
-        var foodInstructions = response.meals[0].strInstructions;
-        var foodTime = response.meals[0].strCategory;
+//         var foodIngredients = [];
+//         var foodMeasures = [];
+//         var mealName = response.meals[0].strMeal;
+//         var foodPicURL = response.meals[0].strMealThumb;
+//         var foodInstructions = response.meals[0].strInstructions;
+//         var foodTime = response.meals[0].strCategory;
         
 
-        if (foodTime !== "Breakfast") {
-            return console.log("end function");
-        } else {
+//         if (foodTime !== "Breakfast") {
+//             return console.log("end function");
+//         } else {
             
             
 
 
 
-//ingredients range from 1-20
-        for (i = 1; i < 20; i++) {
-            if (response.meals[0]["strIngredient" + i] !== null) {
-                foodIngredients.push(response.meals[0]["strIngredient" + i]);
-            }
-        }
+// //ingredients range from 1-20
+//         for (i = 1; i < 20; i++) {
+//             if (response.meals[0]["strIngredient" + i] !== null) {
+//                 foodIngredients.push(response.meals[0]["strIngredient" + i]);
+//             }
+//         }
 
-        for (i = 1; i < 15; i++) {
-            if (response.meals[0]["strMeasure" + i] !==null) {
-                foodMeasures.push(response.meals[0]["strMeasure" + i]);
-            }
-        }
+//         for (i = 1; i < 15; i++) {
+//             if (response.meals[0]["strMeasure" + i] !==null) {
+//                 foodMeasures.push(response.meals[0]["strMeasure" + i]);
+//             }
+//         }
 
-// creating the div to display the meal
-        var mealDisplay = $("<div>");
-        mealDisplay.addClass("Meal");
+// // creating the div to display the meal
+//         var mealDisplay = $("<div>");
+//         mealDisplay.addClass("Meal");
 
-        var foodName = $("<div>");
-        foodName.text(mealName);     
+//         var foodName = $("<div>");
+//         foodName.text(mealName);     
         
-        var foodImg = $("<img>");
-        foodImg.attr("src", foodPicURL);
+//         var foodImg = $("<img>");
+//         foodImg.attr("src", foodPicURL);
 
-        var recipeIngredients = $("<ul>");
-        for(i = 0; i < foodIngredients.length; i++) {
-            recipeIngredientsItem = $("<li>");
-            recipeIngredientsItem.text(foodIngredients[i]);
-            recipeIngredients.append(recipeIngredientsItem);
-        }
+//         var recipeIngredients = $("<ul>");
+//         for(i = 0; i < foodIngredients.length; i++) {
+//             recipeIngredientsItem = $("<li>");
+//             recipeIngredientsItem.text(foodIngredients[i]);
+//             recipeIngredients.append(recipeIngredientsItem);
+//         }
 
-        var recipeMeasures = $("<ul>");
-        for (i = 0; i < foodMeasures.length; i++) {
-            recipeMeasuresItem = $("<li>")
-            recipeMeasuresItem.text(foodMeasures[i]);
-            recipeMeasures.append(recipeMeasuresItem);
-        }
+//         var recipeMeasures = $("<ul>");
+//         for (i = 0; i < foodMeasures.length; i++) {
+//             recipeMeasuresItem = $("<li>")
+//             recipeMeasuresItem.text(foodMeasures[i]);
+//             recipeMeasures.append(recipeMeasuresItem);
+//         }
 
-        console.log("here are the recipe measures: " + response.meals.length);
+//         console.log("here are the recipe measures: " + response.meals.length);
         
-        var recipeGuide = $("<div>");
-        recipeGuide.text(foodInstructions);
+//         var recipeGuide = $("<div>");
+//         recipeGuide.text(foodInstructions);
 
-        mealDisplay.append(foodName);
-        mealDisplay.append(foodImg);
-        mealDisplay.append(recipeIngredients);
-        mealDisplay.append(recipeGuide);
+//         mealDisplay.append(foodName);
+//         mealDisplay.append(foodImg);
+//         mealDisplay.append(recipeIngredients);
+//         mealDisplay.append(recipeGuide);
 
         
 
@@ -77,9 +77,9 @@ function getRecipe() {
 
 
 
-    }})
+//     }})
 
-}
+// }
 
 
 function getRandomRecipe() {
@@ -137,7 +137,7 @@ function getDessert() {
 
 
 
-getRecipe();
+// getRecipe();
 getSearchRecipe();
 getRandomRecipe();
 getDessert();
